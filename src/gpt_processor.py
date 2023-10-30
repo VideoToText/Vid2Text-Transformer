304bb9b015aab24a84cf57757c3bee77be776be4
함윤식 <43927226+Ham3798@users.noreply.github.com>
2023-10-30 19:16:04 +0900

    add env

--- src/gpt_processor.py
+++ src/gpt_processor.py
@@ -1,5 +1,21 @@
+from decouple import config
+import openai
+
+def setup_gpt():
+    """
+    Initialize the OpenAI GPT API with the key from .env file.
+    """
+    api_key = config('OPENAI_API_KEY')
+    openai.api_key = api_key
+
 def process_text_with_gpt(raw_text):
     """
     Given raw text, use the GPT API to structure and refine it.
     """
-    pass
+    response = openai.Completion.create(
+      engine="davinci",
+      prompt=raw_text,
+      max_tokens=500
+    )
+    refined_text = response.choices[0].text.strip()
+    return refined_text
